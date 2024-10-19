7431f158cfb13209be46f9ac8c7dc54bdd059a6d
Matías Quesada <matuquesada999@gmail.com>
2024-10-19 01:42:24 -0300

    Plugins y concepto interfaz gráfica
    Plugins de Maven añadidos para posterior uso en la implementación de interfaz gráfica
    
    Breve intento de reproducción de un menú en Main

--- Respondidos/src/main/java/Juego.java
+++ Respondidos/src/main/java/Juego.java
@@ -1,10 +1,62 @@
-import java.util.ArrayList;
+import javax.swing.*;
+import java.awt.*;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Juego {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Ranking ranking = new Ranking();
+
+        JFrame frame = new JFrame("Respondidos");
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+
+        Font fuenteRespondidos = new Font("Cooper Black", Font.BOLD, 40);
+        JButton button1 = new JButton("Jugar");
+        JButton button2 = new JButton("Salir");
+
+        JLabel etiqueta = new JLabel("RESPONDIDOS");
+        etiqueta.setFont(new Font("Cooper Black", Font.BOLD, 60));  // Fuente más grande
+        etiqueta.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        AtomicInteger valorDevuelto= new AtomicInteger();
+
+        button1.addActionListener(e -> {
+            valorDevuelto.set(obtenerNumero(1));
+
+        });
+        button2.addActionListener(e -> {
+            valorDevuelto.set(obtenerNumero(2));
+
+        });
+
+        int valorNew = valorDevuelto.get();
+
+        button1.setAlignmentX(Component.CENTER_ALIGNMENT);
+        button2.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        button1.setMaximumSize(new Dimension(300, 200));
+        button2.setMaximumSize(new Dimension(300, 200));
+
+        panel.add(Box.createVerticalStrut(50));  // Espacio superior
+        panel.add(etiqueta);
+        panel.add(Box.createVerticalStrut(80));  // Espacio entre texto y botones
+        panel.add(button1);
+        panel.add(Box.createVerticalStrut(30));  // Espacio entre botones
+        panel.add(button2);
+        panel.add(Box.createVerticalGlue());
+
+
+        panel.setBackground(Color.lightGray);
+
+        frame.add(panel);
+        frame.setSize(600, 700);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        /*
 
         Jugador samu = new Jugador("Samu", 0);
         ranking.agregarJugador(samu);
@@ -19,7 +71,11 @@ public class Juego {
         Partida partida = new Partida(new ArrayList<>(), null, samu);
 
         partida.iniciarPartida(samu);
+*/
+    }
 
+    private static int obtenerNumero(int valor) {
+        return valor; // Puedes cambiar este valor a lo que necesites
     }
 
 
